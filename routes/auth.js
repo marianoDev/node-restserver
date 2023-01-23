9985ffe0f78aa5668c43b8b302ab829e3936f488
mariano <mariano.a.rodriguez@accenture.com>
2023-01-23 17:08:06 -0300

    section-11

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { login } = require('../controllers/auth');
+const { login, googleSignin } = require('../controllers/auth');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
@@ -11,5 +11,10 @@ router.post( '/login',
     check('password', 'La contraseña es obligatoria').not().isEmpty(),
     validarCampos
 ,login );
+
+router.post( '/google', 
+    check('id_token', 'El token de google es necesario').not().isEmpty(),
+    validarCampos
+, googleSignin );
 
 module.exports = router;
